ff1cc0139533cbda9919ac26f8602987d2fcd260
prashnat123 <95103887+prashnat123@users.noreply.github.com>
2022-01-21 01:42:01 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+import pickle
+
+app=Flask(__name__)
+model=pickle.load(open('model.pickle','rb'))
+@app.route('/')
+def home():
+    return "<center><h1>TV Sales Predictor</h1></center"
+
+@app.route('/form', methods=['GET','POST'])
+def input_tv():
+    
+    if request.method=='POST':
+        tv_sales=request.form.get("tv sales")
+        tv_list=[[]]
+        tv_list[0].append(float(tv_sales))
+        result=model.predict(tv_list)
+        return "prediction:" + str(result)
+    return render_template('index.html')
+if __name__=='__main__':
+    app.run()
+
